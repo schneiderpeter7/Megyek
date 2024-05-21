c23d8cdeb88a2488d60d285c6e0d59f3f939e885
peter.schneiderr <schpeti7@gmail.com>
2024-05-21 21:34:26 +0200

    kesz a korabbi

--- Megyek/Controllers/KorabbiController.cs
+++ Megyek/Controllers/KorabbiController.cs
@@ -7,24 +7,11 @@ namespace Megyek.Controllers
 {
     public class KorabbiController : Controller
     {
-        public IActionResult KorabbiEredmeny()
-        {
-            return View();
-        }
-        /*public IActionResult KorabbiEredmeny()
+        public IActionResult KorabbiEredmeny(String ev)
         {
             ServicePointManager.Expect100Continue = true;
             ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
-            string json = new WebClient().DownloadString("https://3afae5be-a069-4cdd-962f-9d8feb338a68.mock.pstmn.io/Megyek/Korabbi");
-            List<KorabbiValasztasModel> customers = JsonConvert.DeserializeObject<List<KorabbiValasztasModel>>(json);
-            return View(customers);
-        }*/
-        public IActionResult KorabbiEredmenyek(String ev)
-        {
-            
-            ServicePointManager.Expect100Continue = true;
-            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
-            string json = new WebClient().DownloadString($"https://3afae5be-a069-4cdd-962f-9d8feb338a68.mock.pstmn.io/Megyek/Korabbi{ev}");
+            string json = new WebClient().DownloadString($"https://3afae5be-a069-4cdd-962f-9d8feb338a68.mock.pstmn.io/Megyek/Korabbi?ev={ev}");
             List<KorabbiValasztasModel> customers = JsonConvert.DeserializeObject<List<KorabbiValasztasModel>>(json);
             return View(customers);
         }
