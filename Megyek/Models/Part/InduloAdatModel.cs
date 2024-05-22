190c52c4b6db6e898994e283168a08a896b30dd9
peter.schneiderr <schpeti7@gmail.com>
2024-05-22 10:05:31 +0200

    Felraktam a partokat és az utolso valasztast is

---
+++ Megyek/Models/Part/InduloAdatModel.cs
@@ -0,0 +1,14 @@
+﻿using Megyek_API.Models;
+
+namespace Megyek.Models.Part
+{
+    public class InduloAdatModel
+    {
+        public string indulo_neve { get; set; }
+        public string valasztasi_terulet { get; set; }
+        public SzavazatModel szavazatot_kapott { get; set; }
+        public string orszagos_listaja { get; set; }
+        public int? utoljara_nyert { get; set; }
+        public double nyeresi_eselye { get; set; }
+    }
+}
