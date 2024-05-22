e2fb04ef64ff42a4ff8afef128e23f4de7ad59f8
Ispánki Dávid Simon <d.ispanki@gmail.com>
2024-05-22 19:12:53 +0200

    Új modell: korabbi eredemenyek

--- Megyek/Models/Korabbi/ValasztasInduloModel.cs
+++ Megyek/Models/Korabbi/ValasztasInduloModel.cs
@@ -5,9 +5,8 @@ namespace Megyek.Models.Korabbi
     public class ValasztasInduloModel
     {
         public string indulo_neve { get; set; }
-        public string megye { get; set; }
-        public string partja { get; set; }
-        public string orszagos_lista { get; set; }
+        public string valasztokerulet { get; set; }
+        public string orszagoslista { get; set; }
         public SzavazatModel szavazatot_kapott { get; set; }
         public bool nyert { get; set; }
     }
