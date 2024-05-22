806c132277bd94cbe90814f4c8867fe2c9812658
Ispánki Dávid Simon <d.ispanki@gmail.com>
2024-05-22 18:16:22 +0200

    APIURL endpointok fájlba rendezése

--- Megyek/Controllers/KovetkezoController.cs
+++ Megyek/Controllers/KovetkezoController.cs
@@ -12,7 +12,7 @@ namespace Megyek.Controllers
         {
             ServicePointManager.Expect100Continue = true;
             ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
-            string json = new WebClient().DownloadString($"https://3afae5be-a069-4cdd-962f-9d8feb338a68.mock.pstmn.io/Megyek/Kovetkezo?megye={megye}");
+            string json = new WebClient().DownloadString(new APIURL().GetKovetkezo(megye));
             List<KiFogNyerniModel> customers = JsonConvert.DeserializeObject<List<KiFogNyerniModel>>(json);
             return View(customers);
         }
--- Megyek/Controllers/PartController.cs
+++ Megyek/Controllers/PartController.cs
@@ -12,7 +12,7 @@ namespace Megyek.Controllers
          {
              ServicePointManager.Expect100Continue = true;
              ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
-             string json = new WebClient().DownloadString($"https://3afae5be-a069-4cdd-962f-9d8feb338a68.mock.pstmn.io/Megyek/Part?part={part}");
+             string json = new WebClient().DownloadString(new APIURL().GetPart(part));
              List<PartAdataiModel> customers = JsonConvert.DeserializeObject<List<PartAdataiModel>>(json);
              return View(customers);
          }
