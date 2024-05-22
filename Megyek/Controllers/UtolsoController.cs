4a1386736643a24f9dcbb265d10e97800c5a3b76
peter.schneiderr <schpeti7@gmail.com>
2024-05-22 20:10:52 +0200

    utolso is kesz

--- Megyek/Controllers/UtolsoController.cs
+++ Megyek/Controllers/UtolsoController.cs
@@ -7,13 +7,13 @@ namespace Megyek.Controllers
 {
     public class UtolsoController : Controller
     {
-        public IActionResult UtolsoValasztas(string utolso)
+        public IActionResult UtolsoValasztas(string valasztokerulet)
         {
             ServicePointManager.Expect100Continue = true;
             ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
             try
             {
-                string json = new WebClient().DownloadString(new APIURL().GetUtolso(utolso));
+                string json = new WebClient().DownloadString(new APIURL().GetUtolso(valasztokerulet));
                 List<UtolsoValasztasModel> customers = JsonConvert.DeserializeObject<List<UtolsoValasztasModel>>(json);
                 return View(customers);
             } catch (Exception ex)
